f7b88b93921b409da6c17b09335d8890191e4aca
Eny02 <105815202+Eny02@users.noreply.github.com>
2023-05-22 22:51:57 +0200

    Update variables.tf

--- azure-blob-storage/variables.tf
+++ azure-blob-storage/variables.tf
@@ -1,1 +1,20 @@
+variable "resource_group_name" {
+  type    = string
+  default = "MojaGrupa"
+}
+
+variable "storage_account_name" {
+  type    = string
+  default = "MojStorage"
+}
+
+variable "container_name" {
+  type    = string
+  default = "MojKontener"
+}
+
+variable "blob_name" {
+  type    = string
+  default = "MojBlob"
+}
 
