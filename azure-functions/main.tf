fc0989cf2e46b287e13609f9ea619adfaf28d34d
Eny02 <105815202+Eny02@users.noreply.github.com>
2023-05-22 22:41:43 +0200

    Update main.tf

--- azure-functions/main.tf
+++ azure-functions/main.tf
@@ -9,34 +9,34 @@ data "azurerm_storage_account" "przyklad2StorageAccount" {
 }
 
 output "storage_account_tier" {
-  value = data.azurerm_storage_account.example.account_tier
+  value = data.przyklad2StorageAccount.example.account_tier
 }
 
 resource "azurerm_service_plan" "przyklad3ServicePlan" {
   name                = "przyklad3ServicePlan"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = przykład1ResourceGroup.example.name
+  location            = przykład1ResourceGroup.example.location
   os_type             = "Linux"
   sku_name            = "P1v2"
 }
 
 resource "azurerm_linux_function_app" "przyklad4LinuxApp" {
   name                = "przyklad4LinuxApp"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = przykład1ResourceGroup.example.name
+  location            = przykład1ResourceGroup.example.location
 
-  storage_account_name       = azurerm_storage_account.example.name
-  storage_account_access_key = azurerm_storage_account.example.primary_access_key
-  service_plan_id            = azurerm_service_plan.example.id
+  storage_account_name       = przyklad2StorageAccount.example.name
+  storage_account_access_key = przyklad2StorageAccount.example.primary_access_key
+  service_plan_id            = przyklad3ServicePlan.example.id
 
   site_config {}
 }
 
 resource "azurerm_function_app" "przyklad5FunctionApp" {
   name                       = "przyklad5FunctionApp"
-  location                   = azurerm_resource_group.example.location
-  resource_group_name        = azurerm_resource_group.example.name
-  app_service_plan_id        = azurerm_app_service_plan.example.id
-  storage_account_name       = azurerm_storage_account.example.name
-  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+  location                   = przykład1ResourceGroup.example.location
+  resource_group_name        = przykład1ResourceGroup.example.name
+  app_service_plan_id        = przyklad3ServicePlan.example.id
+  storage_account_name       = przyklad2StorageAccount.example.name
+  storage_account_access_key = przyklad2StorageAccount.example.primary_access_key
 }
