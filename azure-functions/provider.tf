c26757dd63c79ce2438aac05d440d06aee67f7ab
Eny02 <105815202+Eny02@users.noreply.github.com>
2023-05-22 22:47:31 +0200

    Update provider.tf

--- azure-functions/provider.tf
+++ azure-functions/provider.tf
@@ -1,12 +1,4 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
-  }
-}
-
 provider "azurerm" {
+  version = "=3.0.0"
   features {}
 }
